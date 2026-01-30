646ed7860017e0194888255619efa1ddb726bede
Bartlomiej-Ejzert-247652 <247652@edu.p.lodz.pl>
2026-01-30 22:27:40 +0100

    tablet movement

--- Assets/Scripts/FPPController.cs
+++ Assets/Scripts/FPPController.cs
@@ -4,69 +4,93 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class FPPController : MonoBehaviour
 {
-    [Header("Movement Settings")]
-    public bool groundedPlayer = true;
-    public float moveSpeed = 6f;
-    public float jumpHeight = 2f;
-    public float gravity = -9.81f;
+	[Header("Movement Settings")]
+	public bool groundedPlayer = true;
+	public float moveSpeed = 6f;
+	public float jumpHeight = 2f;
+	public float gravity = -9.81f;
 
-    [Header("Mouse Settings")]
-    public Transform cameraTransform;
-    public float mouseSensitivity = 200f;
-    public float maxLookAngle = 80f;
+	[Header("Mouse Settings")]
+	public Transform cameraTransform;
+	public float mouseSensitivity = 200f;
+	public float maxLookAngle = 80f;
 
-    private CharacterController controller;
-    private Vector3 velocity;
-    private float verticalLookRotation = 0f;
+	[Header("Tablet")]
+	public Tablet tablet;
 
-    void Start()
-    {
-        controller = GetComponent<CharacterController>();
-        Cursor.lockState = CursorLockMode.Locked;
-    }
+	private CharacterController controller;
+	private Vector3 velocity;
+	private float verticalLookRotation = 0f;
 
-    void Update()
-    {
-        HandleMovement();
-        HandleMouseLook();
-    }
+	// Zmienna stanu tabletu
+	private bool isTabletVisible = false;
 
-    void HandleMovement()
-    {
-        // Ground check
-        groundedPlayer = controller.isGrounded;
+	void Start()
+	{
+		controller = GetComponent<CharacterController>();
+		Cursor.lockState = CursorLockMode.Locked;
 
-        if (groundedPlayer && velocity.y < 0)
-        {
-            velocity.y = 0f;
-        }
+		isTabletVisible = false;
+		if (tablet != null) tablet.SetState(isTabletVisible);
+	}
 
-        // Input
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+	void Update()
+	{
+		HandleMovement();
+		HandleMouseLook();
+		TabletInput();
+	}
 
-        Vector3 move = transform.right * x + transform.forward * z;
-        controller.Move(move * moveSpeed * Time.deltaTime);
+	void HandleMovement()
+	{
+		// Ground check
+		groundedPlayer = controller.isGrounded;
 
-        // Jump
-        if (Input.GetButtonDown("Jump") && groundedPlayer)
-        {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-        }
-        // Gravity
-            velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
-    }
+		if (groundedPlayer && velocity.y < 0)
+		{
+			velocity.y = 0f;
+		}
 
-    void HandleMouseLook()
-    {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+		// Input
+		float x = Input.GetAxis("Horizontal");
+		float z = Input.GetAxis("Vertical");
 
-        verticalLookRotation -= mouseY;
-        verticalLookRotation = Mathf.Clamp(verticalLookRotation, -maxLookAngle, maxLookAngle);
+		Vector3 move = transform.right * x + transform.forward * z;
+		controller.Move(move * moveSpeed * Time.deltaTime);
 
-        cameraTransform.localRotation = Quaternion.Euler(verticalLookRotation, 0f, 0f);
-        transform.Rotate(Vector3.up * mouseX);
-    }
+		// Jump
+		if (Input.GetButtonDown("Jump") && groundedPlayer)
+		{
+			velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+		}
+
+		// Gravity
+		velocity.y += gravity * Time.deltaTime;
+		controller.Move(velocity * Time.deltaTime);
+	}
+
+	void HandleMouseLook()
+	{
+		float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+		float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+		verticalLookRotation -= mouseY;
+		verticalLookRotation = Mathf.Clamp(verticalLookRotation, -maxLookAngle, maxLookAngle);
+
+		cameraTransform.localRotation = Quaternion.Euler(verticalLookRotation, 0f, 0f);
+		transform.Rotate(Vector3.up * mouseX);
+	}
+
+	public void TabletInput()
+	{
+		if (Input.GetKeyDown(KeyCode.F))
+		{
+			if (tablet != null)
+			{
+				isTabletVisible = !isTabletVisible;
+
+				tablet.SetState(isTabletVisible);
+			}
+		}
+	}
 }
