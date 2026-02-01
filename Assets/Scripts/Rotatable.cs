94dd05165a20eb3bd1c956fdd57478c391fa7b98
Bartlomiej-Ejzert-247652 <247652@edu.p.lodz.pl>
2026-02-01 03:20:24 +0100

    rotato fix

--- Assets/Scripts/Rotatable.cs
+++ Assets/Scripts/Rotatable.cs
@@ -2,22 +2,41 @@ using UnityEngine;
 
 public class Rotatable : MonoBehaviour
 {
-	public float rotateFactor = 2.5f;
-	private float rotateSpeed = 0.0f;
+	public float rotateFactor = 90.0f;
+
+	public bool useGlobalUp = true;
+
+	private float currentSpeed = 0.0f;
 
 	void OnEnable()
 	{
-		rotateSpeed = rotateFactor;
+		currentSpeed = rotateFactor;
 	}
 
 	void OnDisable()
 	{
-		rotateSpeed = 0.0f;
+		currentSpeed = 0.0f;
 	}
 
 	void Update()
 	{
-		gameObject.transform.Rotate(0.0f, rotateSpeed * Time.deltaTime, 0.0f);	
+		if (useGlobalUp)
+		{
+			transform.Rotate(Vector3.up * currentSpeed * Time.deltaTime, Space.World);
+		} else
+		{
+			transform.Rotate(0f, currentSpeed * Time.deltaTime, 0f, Space.Self);
+		}
 	}
 
+	// Dodatkowy podgl¹d w edytorze, ¿ebyœ widzia³ oœ obrotu
+	void OnDrawGizmosSelected()
+	{
+		Gizmos.color = useGlobalUp ? Color.green : Color.yellow;
+		Vector3 axis = useGlobalUp ? Vector3.up : transform.up;
+
+		// Rysuje liniê o d³ugoœci 2m pokazuj¹c¹ oœ obrotu
+		Gizmos.DrawLine(transform.position, transform.position + axis * 2f);
+		Gizmos.DrawSphere(transform.position + axis * 2f, 0.1f);
+	}
 }
